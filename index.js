173be6a54658d414fe637e7db0221c3130cdb1d5
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2023-11-29 17:44:29 +0300

    Find the position! CodeWars

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-function powersOfTwo(n){
-    let res =[];
-    if (n===0){
-        return [1];
-    }else {
-        for (let i = 0; i <= n; i++) {
-            res.push(Math.pow(2,i));
-        }
-        return res;
+function position(letter){
+    let obj = {
+      	A:  1,
+      	B:  2,
+      	C:  3,
+      	D:  4,
+      	E:  5,
+      	F:  6,
+      	G:  7,
+      	H:  8,
+      	I:  9,
+      	J:  10,
+      	K:  11,
+      	L:  12,
+      	M:  13,
+      	N:  14,
+      	O:  15,
+      	P:  16,
+      	Q:  17,
+      	R:  18,
+      	S:  19,
+      	T:  20,
+      	U:  21,
+      	V:  22,
+      	W:  23,
+      	X:  24,
+      	Y:  25,
+      	Z:  26,
     }
+    let res = 0;
+    for (const objKey in obj) {
+        if (letter.toUpperCase() === objKey){
+           res=obj[objKey]
+        }
+    }
+    return `Position of alphabet: ${res}`;
 }
 
-console.log(powersOfTwo(4));
+console.log(position("a"));
