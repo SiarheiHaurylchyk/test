b4e0086d949f69527f408a43b713c774de981708
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2023-12-02 00:47:09 +0300

    Sum of the first nth term of Series CodeWars

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-class Kata {
-    static getVolumeOfCuboid(length, width, height) {
-        return length*width*height;
+function SeriesSum(n)
+{
+    if(n===0){
+        return "0.00";
     }
+    let delitel = 4;
+    let res =0;
+    for(let i = 0; i<=n;i++){
+        if (i>1){
+            res+= 1/delitel;
+            delitel+=3;
+        }
+
+    }
+    res+=1;
+   return  res.toFixed(2).toString();
 }
 
-console.log(position("a"));
+console.log(SeriesSum(2))
