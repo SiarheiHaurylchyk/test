0f3281560ffb7b31a6eb3f4993ad726dba1c132d
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2023-12-02 01:05:05 +0300

    Add Length CodeWars

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function SeriesSum(n)
-{
-    if(n===0){
-        return "0.00";
-    }
-    let delitel = 4;
-    let res =0;
-    for(let i = 0; i<=n;i++){
-        if (i>1){
-            res+= 1/delitel;
-            delitel+=3;
+function addLength(str) {
+    let arr = str.split("");
+    let str1 = "";
+    let res =[];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]!==" "){
+            str1+=arr[i];
         }
-
+        else {
+            str1+=" "+str1.length;
+            res.push(str1);
+            str1="";
+        }
     }
-    res+=1;
-   return  res.toFixed(2).toString();
+    res.push(str1+" "+str1.length)
+    return res
 }
 
-console.log(SeriesSum(2))
+console.log(addLength('apple ban'))
